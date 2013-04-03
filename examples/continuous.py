3abf0e48fad02aab3781bc2a2a39a62523b0b434
Patrick Forringer <patrick@forringer.com>
2013-04-02 19:04:30 -0500

    some comments

--- examples/continuous.py
+++ examples/continuous.py
@@ -15,3 +15,27 @@ while (True):
         print('set speed %s' % (speed) )
         wheel.setSpeed(speed)
         sleep(1)
+
+
+
+# wheel = ContinuousConst(pwm, 0)
+# wheel2 = ContinuousConst(pwm, 1)
+#
+# wheel.set(0)
+# # wheel2.set(0)
+#
+# dur = 10
+# freq = 30
+# factor = 2 * pi * freq/8000
+#
+# # while (True):
+# for seg in range(10 * dur):
+#     # sine wave calculations
+#     sin_seg = sin(seg * factor)
+#     # sin_seq
+#     deg = int((sin_seg))
+#     wheel.set(deg)
+#     print "%s" % deg
+#     sleep(0.05)
+#
+# wheel.set(0)
