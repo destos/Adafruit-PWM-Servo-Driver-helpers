5318239529b5b86b839025ae369838eb29b6c9ed
Patrick Forringer <patrick@forringer.com>
2013-05-15 00:16:38 -0500

    first untested crack at the proximity/light sensor

---
+++ adafruit/hardware/sensors/vcnl4000.py
@@ -0,0 +1,72 @@
+import time
+
+from adafruit.i2c import Adafruit_I2C
+
+class VCNL4000(object):
+    i2c = None
+
+    __ADDRESS 0x13
+
+    # commands and constants
+    __COMMAND           = 0x80
+    __PRODUCTID         = 0x81
+    __IRLED             = 0x83
+    __AMBIENTPARAMETER  = 0x84
+    __AMBIENTDATA       = 0x85
+    __PROXIMITYDATA     = 0x87
+    __SIGNALFREQ        = 0x89
+    __PROXINITYADJUST   = 0x8A
+
+    __3M125     = 0
+    __1M5625    = 1
+    __781K25    = 2
+    __390K625   = 3
+    __FREQUENCIES = ((self.__3M125, '3.125 MHz'),
+                     (self.__1M5625, '1.5625 MHz'),
+                     (self.__781K25, '781.25 KHz'),
+                     (self.__390K625, '390.625 KHz'))
+        
+    __MEASUREAMBIENT    = 0x10
+    __MEASUREPROXIMITY  = 0x08
+    __AMBIENTREADY      = 0x40
+    __PROXIMITYREADY    = 0x20
+    
+    def __init__(self, address=0x40, debug=False):
+        self.i2c = Adafruit_I2C(address, debug=debug)
+        self.address = address
+        self.debug = debug
+
+        rev = self.i2c.readU8(self.__PRODUCTID)
+        if (rev and 0xf0) != 0x10:
+            raise Exception('no sensor found on address: %s' % self.address)
+
+        # set to 20 * 10mA = 200mA
+        self.i2c.write8(self.__IRLED, 20)
+        if self.debug:
+            print 'current = %smA' % self.i2c.read8(self.__IRLED) * 10
+        
+        # TODO: be able to set freqency
+        # self.write8(self.__SIGNALFREQ, 3)
+        if self.debug:
+            freq = self.i2c.read8(self.__SIGNALFREQ)
+            print "proximity measurement frequency = %s" % self.__FREQUENCIES[freq]
+
+    def readProximity(self):
+        self.i2c.write8(self.__COMMAND, self.__MEASUREPROXIMITY)
+        while True:
+            result = self.i2c.read8(self.__COMMAND)
+            # proximity sensor has been freed
+            if result & self.__PROXIMITYREADY:
+                return self.i2c.read16(self.__PROXIMITYDATA)
+            time.sleep(1)
+
+    # TODO: similar to proximity, maybe keep it dry?
+    def readAmbient(self):
+        self.i2c.write8(self.__COMMAND, self.__MEASUREAMBIENT)
+        while True:
+            result = self.i2c.read8(self.__COMMAND)
+            # ambient sensor has been freed
+            if result & self.__AMBIENTREADY:
+                return self.i2c.read16(self.__AMBIENTDATA)
+            time.sleep(1)
+        
