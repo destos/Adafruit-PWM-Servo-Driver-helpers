821ca9630b8c33e0d0169f0a8379fd6f71659f1f
Patrick Forringer <patrick@forringer.com>
2013-05-16 21:28:21 -0500

    werking on the railroad

--- adafruit/hardware/sensors/vcnl4000.py
+++ adafruit/hardware/sensors/vcnl4000.py
@@ -2,6 +2,7 @@ import time
 
 from adafruit.i2c import Adafruit_I2C
 
+
 class VCNL4000(object):
     i2c = None
 
@@ -21,35 +22,37 @@ class VCNL4000(object):
     __1M5625    = 1
     __781K25    = 2
     __390K625   = 3
-    __FREQUENCIES = ((self.__3M125, '3.125 MHz'),
-                     (self.__1M5625, '1.5625 MHz'),
-                     (self.__781K25, '781.25 KHz'),
-                     (self.__390K625, '390.625 KHz'))
-        
+    __FREQUENCIES = (
+        (__3M125, '3.125 MHz'),
+        (__1M5625, '1.5625 MHz'),
+        (__781K25, '781.25 KHz'),
+        (__390K625, '390.625 KHz'))
+
     __MEASUREAMBIENT    = 0x10
     __MEASUREPROXIMITY  = 0x08
     __AMBIENTREADY      = 0x40
     __PROXIMITYREADY    = 0x20
     
-    def __init__(self, address=0x40, debug=False):
+    def __init__(self, address=0x13, debug=False):
         self.i2c = Adafruit_I2C(address, debug=debug)
         self.address = address
         self.debug = debug
 
         rev = self.i2c.readU8(self.__PRODUCTID)
-        if (rev and 0xf0) != 0x10:
+        if (rev & 0xf0) != 0x10:
             raise Exception('no sensor found on address: %s' % self.address)
-
+        
+        # make configurable?
         # set to 20 * 10mA = 200mA
         self.i2c.write8(self.__IRLED, 20)
         if self.debug:
             print 'current = %smA' % self.i2c.read8(self.__IRLED) * 10
-        
+
         # TODO: be able to set freqency
         # self.write8(self.__SIGNALFREQ, 3)
         if self.debug:
             freq = self.i2c.read8(self.__SIGNALFREQ)
-            print "proximity measurement frequency = %s" % self.__FREQUENCIES[freq]
+            print "proximity measurement frequency = %s" % self.__FREQUENCIES[freq][1]
 
     def readProximity(self):
         self.i2c.write8(self.__COMMAND, self.__MEASUREPROXIMITY)
@@ -69,4 +72,6 @@ class VCNL4000(object):
             if result & self.__AMBIENTREADY:
                 return self.i2c.read16(self.__AMBIENTDATA)
             time.sleep(1)
-        
+
+
+FREQUENCIES = dict((v, k) for k, v in VCNL4000.__FREQUENCIES)
